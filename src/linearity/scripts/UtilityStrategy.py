ec6faa8c9c41c0a930232e0e281bde029aba2eb8
Sable <hrk7570@rit.edu>
2021-04-06 15:49:50 -0400

    added base case to utility strategy

--- src/linearity/scripts/UtilityStrategy.py
+++ src/linearity/scripts/UtilityStrategy.py
@@ -39,23 +39,41 @@ class UtilityStrategy:
                     return atomic_waypoint
 
     def getNext(self, waypoints, visited, dronestate: DroneState):
-        home_wp = self.getReturnWaypoint(waypoints)
-        home_cost = self.getWaypointCosts(home_wp, dronestate)
+        next_wp = None
+        next_utility = -100
+        if visited.len() = 0:
 
-        # default choice is to go home, and default utility is the cost of going home without attaining a reward
-        next_utility = 0 - home_cost
-        next_wp = home_wp
+            for atomic_waypoint in waypoints:
+                if atomic_waypoint != self.getReturnWaypoint(waypoints):
+                    wp_reward = atomic_waypoint.getRewardValue()
+                    wp_dist = self._calcDistance(atomic_waypoint, dronestate.getdata())
 
-        for atomic_waypoint in waypoints:
-            if atomic_waypoint not in visited:
-                wp_cost = self.getWaypointCosts(atomic_waypoint, dronestate)
-                wp_reward = atomic_waypoint.getRewardValue()
-                wp_utility = wp_reward - wp_cost
+                    wp_utility = wp_reward / wp_dist
 
-                if wp_utility > next_utility:
-                    next_wp = atomic_waypoint
-                    next_utility = wp_utility
+                    if wp_utility > next_utility:
+                        next_wp = atomic_waypoint
+                        next_utility = wp_utility
+            
+            print("waypoint was chosen and it is worth", next_utility, "points")
+            return next_wp
 
-        print("waypoint was chosen and it is worth", next_utility, "points")
-        return next_wp
+        else:
+            home_wp = self.getReturnWaypoint(waypoints)
+            home_cost = self.getWaypointCosts(home_wp, dronestate)
+
+            next_wp = home_wp
+            next_utility = home_wp.getRewardValue() - home_cost
+
+            for atomic_waypoint in waypoints:
+                if atomic_waypoint not in visited:
+                    wp_cost = self.getWaypointCosts(atomic_waypoint, dronestate)
+                    wp_reward = atomic_waypoint.getRewardValue()
+                    wp_utility = wp_reward - wp_cost
+
+                    if wp_utility > next_utility:
+                        next_wp = atomic_waypoint
+                        next_utility = wp_utility
+
+            print("waypoint was chosen and it is worth", next_utility, "points")
+            return next_wp
               
